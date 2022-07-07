ad4a348976b994e0fc5ec298e4aa90943386ce58
jkhales <jonathan@postera.ai>
2022-07-07 11:41:08 -0400

    ketcher demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Editor } from "ketcher-react";
+import { StandaloneStructServiceProvider } from "ketcher-standalone";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Editor
+      staticResourcesUrl={process.env.PUBLIC_URL}
+      structServiceProvider={new StandaloneStructServiceProvider()}
+      errorHandler={console.error}
+    />
   );
 }
 
